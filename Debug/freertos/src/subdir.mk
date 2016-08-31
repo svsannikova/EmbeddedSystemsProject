5319216d9095f4f391c8a2e517a47b6a9ba15285
Jaakko Voutilainen <jaakko.voutilainen@metropolia.fi>
2016-08-31 22:02:08 +0300

    Basic stuff
    Just included the freertos library, and made settings ready. Also merged
    simple pressButton -thread to test functionality, and tick counter
    -thread. If you press SW1 button, it should show red led.

---
+++ Debug/freertos/src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../freertos/src/FreeRTOSCommonHooks.c \
+../freertos/src/croutine.c \
+../freertos/src/event_groups.c \
+../freertos/src/heap_3.c \
+../freertos/src/list.c \
+../freertos/src/port.c \
+../freertos/src/queue.c \
+../freertos/src/tasks.c \
+../freertos/src/timers.c 
+
+OBJS += \
+./freertos/src/FreeRTOSCommonHooks.o \
+./freertos/src/croutine.o \
+./freertos/src/event_groups.o \
+./freertos/src/heap_3.o \
+./freertos/src/list.o \
+./freertos/src/port.o \
+./freertos/src/queue.o \
+./freertos/src/tasks.o \
+./freertos/src/timers.o 
+
+C_DEPS += \
+./freertos/src/FreeRTOSCommonHooks.d \
+./freertos/src/croutine.d \
+./freertos/src/event_groups.d \
+./freertos/src/heap_3.d \
+./freertos/src/list.d \
+./freertos/src/port.d \
+./freertos/src/queue.d \
+./freertos/src/tasks.d \
+./freertos/src/timers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+freertos/src/%.o: ../freertos/src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__NEWLIB__ -DDEBUG -D__CODE_RED -DDONT_ENABLE_SWVTRACECLK -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\Users\Jaakko\Documents\LPCXpresso_8.2.0_647\workspace\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\Users\Jaakko\Documents\LPCXpresso_8.2.0_647\workspace\lpc_chip_15xx\inc" -I"C:\Users\Jaakko\Documents\LPCXpresso_8.2.0_647\workspace\EmbeddedSystemsProject\src" -I"C:\Users\Jaakko\Documents\LPCXpresso_8.2.0_647\workspace\EmbeddedSystemsProject\freertos\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
