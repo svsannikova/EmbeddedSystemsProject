649c94462fe73a3b81327ffdf6e9b53616ccc524
jaakkovo <jaakko.voutilainen@metropolia.fi>
2016-09-02 10:12:52 +0300

    Basic stuff ver.2
    Last time all the code didnt commit, so attempt number 2.

--- src/EmbeddedSystemsProject.cpp
+++ src/EmbeddedSystemsProject.cpp
@@ -6,7 +6,11 @@
  Copyright   : $(copyright)
  Description : main definition
 ===============================================================================
-*/
+ */
+
+#include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 #if defined (__USE_LPCOPEN)
 #if defined(NO_BOARD_LIB)
@@ -19,30 +23,58 @@
 #include <cr_section_macros.h>
 
 // TODO: insert other include files here
-// THIS LINE CHANGED
-// TODO: insert other definitions and declarations here
+
+/* UART (or output) thread */
+static void vUARTTask(void *pvParameters) {
+	int tickCnt = 0;
+
+	while (1) {
+		tickCnt++;
+		if (!Chip_GPIO_GetPinState(LPC_GPIO, 0, 17)){
+			Board_LED_Set(0, true);
+		}else{
+			Board_LED_Set(0, false);
+		}
+
+		DEBUGOUT("Tick: %d \r\n", tickCnt);
+
+		/* About a 1s delay here */
+		vTaskDelay(configTICK_RATE_HZ/6);
+	}
+}
+
 
 int main(void) {
 
 #if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
+	// Read clock settings and update SystemCoreClock variable
+	SystemCoreClockUpdate();
 #if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
+	// Set up and initialize all required blocks and
+	// functions related to the board hardware
+	Board_Init();
+	// Set the LED to the state of "On"
 #endif
 #endif
 
-    // TODO: insert code here
+	uint32_t pm = IOCON_DIGMODE_EN;
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
-    return 0 ;
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 17, IOCON_DIGMODE_EN);
+	Chip_GPIO_SetPinDIRInput(LPC_GPIO, 0, 17);
+
+	/* UART output thread, simply counts seconds */
+	xTaskCreate(vUARTTask, "vTaskUart",
+			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	// Force the counter to be placed into memory
+	volatile static int i = 0 ;
+	// Enter an infinite loop, just incrementing a counter
+	while(1) {
+		i++ ;
+	}
+	return 0 ;
 }
